c21e541d06f9585f7dd2348774fb02f573ca09ee
sdoerr1 <sdoerr1@jhu.edu>
2015-12-27 00:01:51 -0500

    edited android manifest

---
+++ MessagingTutorial/app/src/androidTest/java/sdoerr1/messagingtutorial/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sdoerr1.messagingtutorial;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
