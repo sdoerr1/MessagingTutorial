506befcbf5f4f3ee551003d902bf5d10335b855a
sdoerr1 <sdoerr1@jhu.edu>
2015-12-27 00:37:02 -0500

    Added activity and defined Login activity

---
+++ MessagingTutorial/app/src/main/java/sdoerr1/messagingtutorial/LoginActivity.java
@@ -0,0 +1,15 @@
+package sdoerr1.messagingtutorial;
+import android.app.Activity;
+import android.os.Bundle;
+
+import sdoerr1.messagingtutorial.R;
+/**
+ * Created by Sophia on 12/27/2015.
+ */
+public class LoginActivity extends Activity {
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_login);
+  }
+}
